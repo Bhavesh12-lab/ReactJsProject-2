09051b48d71943d2d38f3b3152152cbe48811d20
Bhavesh12-lab <bsondagar6@gmail.com>
2024-11-30 16:20:14 +0530

    Project-2

---
+++ React-Counter/CounterJSX/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+// import Counter from './counter/Counter.jsx'
+import Counter   from './counter/counter'
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+  
+      // <Home name = {'Hello react js'}/>
+      
+    <Counter name ={'hello react js'} />
+  )
+}
+
+export default App
